6eeb6d7649d6d11d544eb8eb84eb550970d80926
rossjbartlett <rossjbartlett@gmail.com>
2022-08-26 00:34:36 -0600

    Updates

---
+++ precache-manifest.1ae0e113e1a17afc0b03fa9c336bed20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dd29e3677ff6002f66dbee40a756246",
+    "url": "/100-prisoners/index.html"
+  },
+  {
+    "revision": "5b8dbffe51f1fb7b9af4",
+    "url": "/100-prisoners/static/css/2.2729d855.chunk.css"
+  },
+  {
+    "revision": "5b8dbffe51f1fb7b9af4",
+    "url": "/100-prisoners/static/js/2.5da78b50.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/100-prisoners/static/js/2.5da78b50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "525957aeffd341b02d81",
+    "url": "/100-prisoners/static/js/main.d2a664ef.chunk.js"
+  },
+  {
+    "revision": "87c1e4eea1daeca86715",
+    "url": "/100-prisoners/static/js/runtime-main.5eff1306.js"
+  }
+]);
